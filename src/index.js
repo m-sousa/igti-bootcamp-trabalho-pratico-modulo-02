28c02aa239b111b76145bc81d67938e4b32065db
Marcos Sousa <marcos.sousa@outlook.com>
2020-06-07 18:12:16 -0300

    Resoluao das atividades 03 e 04.

--- src/index.js
+++ src/index.js
@@ -5,23 +5,25 @@ const { readFile, writeFile } = promises;
 
 init();
 
-function init() {
+async function init() {
   try {
-    createStateWithCitiesFiles();
-    getNumberOfCitiesPerState("AC");
+    await createStateWithCitiesFiles();
+    await getNumberOfCitiesPerState("AC");
+    await printTopFiveStatesMoreCities();
+    await printTopFiveStatesLessCities();
   } catch (error) {
     console.log(error);
   }
 }
 
 async function createStateWithCitiesFiles() {
-  const estados = await loadFile("Estados.json");
-  const cidades = await loadFile("Cidades.json");
+  const states = await loadFile("Estados.json");
+  const cities = await loadFile("Cidades.json");
 
-  estados.forEach(async (estado) => {
-    let data = cidades.filter((cidade) => cidade.Estado === estado.ID);
+  states.forEach(async (state) => {
+    let data = cities.filter((city) => city.Estado === state.ID);
     try {
-      await writeFile(`files/out/${estado.Sigla}.json`, JSON.stringify(data));
+      await writeFile(`files/out/${state.Sigla}.json`, JSON.stringify(data));
     } catch (error) {
       console.log(error);
     }
@@ -31,10 +33,49 @@ async function createStateWithCitiesFiles() {
 async function getNumberOfCitiesPerState(uf) {
   try {
     const cities = await loadFile(`${uf}.json`, true);
-    console.log(cities.length);
+    return cities.length;
   } catch (error) {
     console.log(error);
   }
+}
+
+async function printTopFiveStatesMoreCities() {
+  const states = await getStatesOrderedByNumberOfCities();
+  const topFiveStates = states
+    .reverse()
+    .slice(0, 5)
+    .map((state) => `${state.uf} - ${state.numberOfCities}`);
+  console.log(topFiveStates);
+}
+
+async function printTopFiveStatesLessCities() {
+  const states = await getStatesOrderedByNumberOfCities();
+  const topFiveStates = states
+    .slice(0, 5)
+    .map((state) => `${state.uf} - ${state.numberOfCities}`);
+  console.log(topFiveStates);
+}
+
+async function getStatesOrderedByNumberOfCities() {
+  const states = await loadFile("Estados.json");
+  let serializedStates = [];
+
+  for (const state of states) {
+    let actualStateNumberOfCities = await getNumberOfCitiesPerState(
+      state.Sigla
+    );
+
+    serializedStates = [
+      ...serializedStates,
+      { uf: state.Sigla, numberOfCities: actualStateNumberOfCities },
+    ];
+  }
+
+  serializedStates = serializedStates.sort(
+    (a, b) => a.numberOfCities - b.numberOfCities
+  );
+
+  return serializedStates;
 }
 
 async function loadFile(fileName, isOutputFile = false) {
